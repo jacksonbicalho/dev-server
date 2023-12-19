4b881ca5c8a79e849fb29d11450cbfd1c6c67c4f
Jackson Bicalho <jacksonbicalho@gmail.com>
2023-12-19 20:23:37 -0300

    teste 22222222222222

--- jest/jest.setup.ts
+++ jest/jest.setup.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2023 Jackson Bicalho.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import fetchMock from 'jest-fetch-mock';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
